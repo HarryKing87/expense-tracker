3e8a9c8776f47944988e457c40e77c467e0deeb8
Harry <46583043+HarryKing87@users.noreply.github.com>
2021-06-25 13:48:11 +0300

    Formatted document & made final commit

--- script.js
+++ script.js
@@ -1,35 +1,26 @@
 let buttonIncomeToggler = document.querySelector(".buttonIncome");
 let buttonOutcomeToggler = document.querySelector(".buttonOutcome");
-let incomeContainer = document.querySelector('.containerIncome');
-let outcomeContainer = document.querySelector('.containerOutcome');
+let incomeContainer = document.querySelector(".containerIncome");
+let outcomeContainer = document.querySelector(".containerOutcome");
 let showTrans = document.querySelector(".button");
 let switchMode = document.querySelector(".switch");
 let toggleIncome = document.getElementById("incomeShow");
 let toggleOutcome = document.getElementById("outcomeShow");
 
 buttonIncomeToggler.addEventListener("click", function () {
-
-          if (buttonIncomeToggler.checked)
-          {
-                    incomeContainer.classList.add('.containerIncome');
-                    incomeContainer.className = "showIncomeOutcome";
-          }
-          else {
-                    incomeContainer.className = "closeForm";
-          }
-          
+  if (buttonIncomeToggler.checked) {
+    incomeContainer.classList.add(".containerIncome");
+    incomeContainer.className = "showIncomeOutcome";
+  } else {
+    incomeContainer.className = "closeForm";
+  }
 });
 
-buttonOutcomeToggler.addEventListener('click', function() {
-          if (buttonOutcomeToggler.checked)
-          {
-                    outcomeContainer.classList.add('.containerOutcome');
-                    outcomeContainer.className = "showIncomeOutcome";
-          }
-          
-
-          else {
-                    outcomeContainer.className = "closeForm";
-          }
+buttonOutcomeToggler.addEventListener("click", function () {
+  if (buttonOutcomeToggler.checked) {
+    outcomeContainer.classList.add(".containerOutcome");
+    outcomeContainer.className = "showIncomeOutcome";
+  } else {
+    outcomeContainer.className = "closeForm";
+  }
 });
-
